538f5c696a74ebca1ac5a2692e4095bf85dac6fd
devifeoma <judithcynthia1@gmail.com>
2022-05-23 13:36:33 +0100

    Add the search button.

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Search() {
+  return (
+    <div>
+      <input placeholder="Search for a gif"/>
+      <button>Search</button>
+    </div>
+  );
+}
+
+export default Search
